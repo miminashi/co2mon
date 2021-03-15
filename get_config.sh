c69c5751214c8e467c3e8e6b235c7d9aa5c3b496
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2021-03-16 04:20:34 +0900

    設定ストアへの読み書きをコンテナを介さないようにした

--- get_config.sh
+++ get_config.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
 #
+# ターゲットの設定ストアから設定値を読出します
+#
 # 使い方:
 #   ./get_config.sh TARGET KEY
 #
 
 set -eu
 
-: "${1}"
-: "${2}"
+. ./config
 
-target="${1}"
-key="${2}"
+# 引数のチェック
+target="${1:?"第1引数にターゲット(ssh接続先)を与えてください"}"
+key="${2:?"第2引数に設定キーを与えてください"}"
+key_file="${CONF_DIR}"/"${key}"
 
-ssh "${target}" "docker run --rm -v /var/local/co2mon:/var/local/co2mon co2mon /workdir/app/get_config.sh \"${key}\""
+ssh "${target}" "cat \"${key_file}\""
