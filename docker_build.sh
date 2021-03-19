9c97a16672660b1c466a78ea0f8be2dc2e193786
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2021-03-20 04:11:31 +0900

    暫定x86_64対応した

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker build --platform linux/arm/v7 -t co2mon .
+docker build --platform linux/arm/v7 -t co2mon .   # Armのマシンはこちら
+#docker build --platform linux/amd64 -t co2mon .   # x86_64のマシンはこちら
