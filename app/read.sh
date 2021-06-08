3b716f33d1a770449f3cccd3f3f4802660723a05
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2021-06-08 12:58:45 +0900

    ログのパスを修正した

--- app/read.sh
+++ app/read.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+set -eu
+
 echo "read.sh started" >&2
 
 LOG_DIR="/var/local/co2mon/DATA/log/co2"  # rotateは別プロセスで
-LOG="${log_dir}/latest"
+LOG="${LOG_DIR}/latest"
 DEFAULT_USB_CO2="/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0"
 
-usb_co2="$(./app/get_config.sh usb_co2)"
+usb_co2="$(./app/get_config.sh usb_co2)" || usb_co2=""
 if [ -z "${usb_co2}" ]; then
   usb_co2="${DEFAULT_USB_CO2}"
 fi
@@ -15,7 +17,6 @@ mkdir -p "${LOG_DIR}"
 
 stty -F "${usb_co2}" raw 9600
 
-cat "${usb_co2}" |
 while read -r l; do
   printf '%s %s\n' "$(date +%s)" "${l}"
-done >> "${LOG}"
+done < "${usb_co2}" >> "${LOG}"
